b812247a49162a03958ac9966cfb16dc4cff9c58
Jorge Villalobos <jorrgee.4l@gmail.com>
2022-05-14 18:15:39 -0700

    refactor(projects): refactored projects files
    refactored projects files and folders along with placing variants into a new file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./components/styles/Global";
 import Nav from "./components/Nav/Nav.js";
 import Home from "./components/Home";
-import Projects from "./components/Projects";
+import Projects from "./components/Projects/Projects";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import ParticlesBack from "./components/ParticlesBack";
@@ -12,33 +11,23 @@ import { ContainerContent } from "./components/styles/ContainerContent.styled";
 import { ContainerNav } from "./components/styles/ContainerNav.styled";
 import "./App.css";
 
-const theme = {
-  colors: {
-    header: "blue",
-    body: "red",
-    footer: "black",
-  },
-};
-
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <>
-        <ParticlesBack />
-        <GlobalStyles />
-        <ContainerNav>
-          <Nav />
-        </ContainerNav>
-        <ContainerContent>
-          <Home />
-          <About />
-          <Resume />
-          <Projects />
-          <Contact />
-          {/* <ScrollUp /> */}
-        </ContainerContent>
-      </>
-    </ThemeProvider>
+    <>
+      <ParticlesBack />
+      <GlobalStyles />
+      <ContainerNav>
+        <Nav />
+      </ContainerNav>
+      <ContainerContent>
+        <Home />
+        <About />
+        <Resume />
+        <Projects />
+        <Contact />
+        {/* <ScrollUp /> */}
+      </ContainerContent>
+    </>
   );
 }
 
---
+++ src/components/Projects/Variants.js
@@ -0,0 +1,19 @@
+export const projectsVariant = {
+  hidden: {
+    y: "20vh",
+    opacity: 0,
+  },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.7,
+    },
+  },
+};
+
+export const singleProjVariant = {
+  hover: {
+    scale: 1.05,
+  },
+};
