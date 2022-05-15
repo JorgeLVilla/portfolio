8db447b623e5a1e2e02a521d2e0b40849c4be9ff
Jorge Villalobos <jorrgee.4l@gmail.com>
2022-05-14 18:29:56 -0700

    refactor(home): refactor home section code
    refactors the code in the home section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import GlobalStyles from "./components/styles/Global";
 import Nav from "./components/Nav/Nav.js";
-import Home from "./components/Home";
+import Home from "./components/Home/Home";
 import Projects from "./components/Projects/Projects";
 import About from "./components/About";
 import Contact from "./components/Contact";
---
+++ src/components/Home/Variants.js
@@ -0,0 +1,27 @@
+export const greetingVariant = {
+  hidden: {
+    y: "20vh",
+    opacity: 0,
+  },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 1.25,
+    },
+  },
+};
+
+export const heroVariant = {
+  hidden: {
+    y: "-20vh",
+    opacity: 0,
+  },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 1.25,
+    },
+  },
+};
